c5d789e01b52d68d25c7d1c10b5cb6206ec965a8
Chloe Steffens <caforonda103@yahoo.com>
2020-06-18 05:38:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,15 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/name' do
-    @name = "My name is Chloe"
+    "My name is Chloe"
   end
   
   get '/hometown' do
-    @hometown = "My hometown is Lake Zurich"
+    "My hometown is Lake Zurich"
   end
   
-  get '/favorite-song'
-    @favorite-song ="My favorite song is 'All the Wine' by The National"
+  get '/favorite-song' do
+    "My favorite song is 'All the Wine' by The National"
   end
   
 end
