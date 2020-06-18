0053afe26b9cac466f1dc2c4618674dc911e6064
Chloe Steffens <caforonda103@yahoo.com>
2020-06-18 05:38:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Chloe"
+  end
+  
+  get '/hometown' do
+    "My hometown is Lake Zurich"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is 'All the Wine' by The National"
+  end
+  
 end
